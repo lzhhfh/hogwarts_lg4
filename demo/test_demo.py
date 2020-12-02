6ca16aeb5b07aa6a48827dcd24a844f5f447333a
lzhhfh <1963566234@qq.com>
2020-12-02 13:58:21 +0800

    add demo

---
+++ demo/test_demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+
+r = requests.get('https://github.com/')
+
+
+
---
+++ demo/test_requests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def test_login():
+    print('test! login!')  # 这不是执行了？
